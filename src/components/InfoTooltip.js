f06df3da93eb3ac36a4889d6c389d77821d4be33
a1x02 <Sashurik2002@gmail.com>
2023-08-31 17:40:59 +0300

    feat: реализовать компоненты защищенного роута и модального окна авторизации

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,32 @@
+import React from "react";
+import authPass from '../images/auth-true.svg'
+import authFail from '../images/auth-false.svg'
+
+function InfoTooltip({isOpen, answer, onClose}) {
+    function handleClose(evt) {
+        if (evt.target.classList.contains('popup_opened')) {
+            onClose()
+            evt.target.classList.remove('popup_opened')
+        }
+    }
+
+    return(
+        <div className={`popup ${isOpen ? "popup_opened" : ""}`}>
+            <div className="popup__container">
+                <img
+                    className="popup__auth-image"
+                    alt={answer.status ? "Авторизация успешна" : "Что-то пошло не так"}
+                    src={answer.status ? authPass : authFail}>
+                </img>
+                <h2 className="popup__title popup__title_auth">{answer.text}</h2>
+                <button
+                    className="popup__close"
+                    type="button"
+                    onClick={onClose}>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default InfoTooltip
